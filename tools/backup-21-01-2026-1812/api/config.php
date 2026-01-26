8f566001063fcb7c52f042a60c4247bbbfc4ef07
irlam <cirlam@gmail.com>
2026-01-26 20:02:04 +0000

    feat: add configuration and multiple plan documents for project backup

---
+++ tools/backup-21-01-2026-1812/api/config.php
@@ -0,0 +1,14 @@
+<?php
+return [
+  'base_url' => 'https://survey.defecttracker.uk',
+  'storage_path' => 'storage',
+
+  'db' => [
+    'host' => '10.35.233.124',
+    'port' => 3306,
+    'dbname' => 'k87747_survey',
+    'user' => 'k87747_defecttracker',
+    'pass' => 'Subaru5554346',
+    'charset' => 'utf8mb4',
+  ],
+];
