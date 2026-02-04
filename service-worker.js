4768195ea006ecdc031e34b36ba425abd8e6c597
irlam <cirlam@gmail.com>
2026-02-04 21:11:38 +0000

    Disable crop export while processing

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* service-worker.js - PWA caching strategy (04/02/2026) */
 
-const STATIC_CACHE = 'survey-pwa-static-v42';
-const RUNTIME_CACHE = 'survey-pwa-runtime-v42';
+const STATIC_CACHE = 'survey-pwa-static-v43';
+const RUNTIME_CACHE = 'survey-pwa-runtime-v43';
 const ASSETS = [
   '/',
   '/index.html',
