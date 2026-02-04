49b6553a54c57a1852666bc1501a1adf9ac4fe3f
irlam <cirlam@gmail.com>
2026-02-04 20:57:59 +0000

    Use noopener for DWG tool popouts

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* service-worker.js - PWA caching strategy (04/02/2026) */
 
-const STATIC_CACHE = 'survey-pwa-static-v30';
-const RUNTIME_CACHE = 'survey-pwa-runtime-v30';
+const STATIC_CACHE = 'survey-pwa-static-v31';
+const RUNTIME_CACHE = 'survey-pwa-runtime-v31';
 const ASSETS = [
   '/',
   '/index.html',
