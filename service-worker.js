5c65aca659ec985c45890ed45cc236315e3ef2a1
irlam <cirlam@gmail.com>
2026-02-04 21:18:37 +0000

    Clean unused variables in convert tool

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* service-worker.js - PWA caching strategy (04/02/2026) */
 
-const STATIC_CACHE = 'survey-pwa-static-v50';
-const RUNTIME_CACHE = 'survey-pwa-runtime-v50';
+const STATIC_CACHE = 'survey-pwa-static-v51';
+const RUNTIME_CACHE = 'survey-pwa-runtime-v51';
 const ASSETS = [
   '/',
   '/index.html',
