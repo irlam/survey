13936b4915d50b6cd985fc482853e69e9b570b0e
irlam <cirlam@gmail.com>
2026-02-04 20:51:42 +0000

    Add list roles to exports grid

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* service-worker.js - PWA caching strategy (04/02/2026) */
 
-const STATIC_CACHE = 'survey-pwa-static-v25';
-const RUNTIME_CACHE = 'survey-pwa-runtime-v25';
+const STATIC_CACHE = 'survey-pwa-static-v26';
+const RUNTIME_CACHE = 'survey-pwa-runtime-v26';
 const ASSETS = [
   '/',
   '/index.html',
