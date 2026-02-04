60ef9688c8500b01af958f7dbcc1bf82d0953552
irlam <cirlam@gmail.com>
2026-02-04 20:40:56 +0000

    Improve annotate tool status a11y

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* service-worker.js - PWA caching strategy (04/02/2026) */
 
-const STATIC_CACHE = 'survey-pwa-static-v17';
-const RUNTIME_CACHE = 'survey-pwa-runtime-v17';
+const STATIC_CACHE = 'survey-pwa-static-v18';
+const RUNTIME_CACHE = 'survey-pwa-runtime-v18';
 const ASSETS = [
   '/',
   '/index.html',
