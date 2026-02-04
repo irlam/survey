c22c35cef6db35595100c040eef07c3a26757715
irlam <cirlam@gmail.com>
2026-02-04 21:03:28 +0000

    Use noopener for trash viewer popout

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* service-worker.js - PWA caching strategy (04/02/2026) */
 
-const STATIC_CACHE = 'survey-pwa-static-v36';
-const RUNTIME_CACHE = 'survey-pwa-runtime-v36';
+const STATIC_CACHE = 'survey-pwa-static-v37';
+const RUNTIME_CACHE = 'survey-pwa-runtime-v37';
 const ASSETS = [
   '/',
   '/index.html',
