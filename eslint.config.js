8340469f31b08241ffae35fa0e02bc65632d1586
irlam <cirlam@gmail.com>
2026-02-22 13:56:32 +0000

    fix: Clean up ESLint warnings

--- eslint.config.js
+++ eslint.config.js
@@ -42,7 +42,7 @@ export default [
       }
     },
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
       "no-console": "off",
       "no-undef": "off",
       "no-implicit-globals": "off",
